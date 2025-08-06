960b2c6f1fd44e91e64d30dd4ac2b387df29c0ba
DREAM <voigkgky@gmail.com>
2025-08-06 20:13:02 +0700

    M

--- next.config.js
+++ next.config.js
@@ -3,8 +3,9 @@ const nextConfig = {
   async rewrites() {
     return [
       { source: '/old-route', destination: '/new-route' }
-    ]
+    ];
   },
+  // config อื่น ๆ
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
